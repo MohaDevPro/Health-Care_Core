766b76374b7e39bf6a783aac57b4cf02f78e3aa8
M <DevITPro@protonmail.com>
2021-04-25 15:00:51 +0300

    init commit

---
+++ Health Care/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Health_Care.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
