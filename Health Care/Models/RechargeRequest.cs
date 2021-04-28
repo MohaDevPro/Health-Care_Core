7d01006b62fd89ca2d59c376e697b83962801dda
ASH <smart.success.sa@gmail.com>
2021-04-28 11:11:46 +0300

    rechargeRequestAdded

---
+++ Health Care/Models/RechargeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Health_Care.Models
+{
+    public class RechargeRequest
+    {
+        public int id { get; set; }
+        public int userId { get; set; }
+        public byte[] BalanceSanad { get; set; } //صورة السند
+        public string rechargeDate { get; set; }
+        public bool ConfirmToAdd { get; set; }
+    }
+}
