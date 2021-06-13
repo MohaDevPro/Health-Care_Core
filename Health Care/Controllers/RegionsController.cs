18d7a1a498077bf1ba2635e69c48f0bd20dc88f7
mohamedbikar <mohamedbikar32@gmail.com>
2021-06-13 11:49:05 +0300

    12

--- Health Care/Controllers/RegionsController.cs
+++ Health Care/Controllers/RegionsController.cs
@@ -69,6 +69,7 @@ namespace Health_Care.Controllers
             {
                 return BadRequest(e.Message);
             }
+        }
         [HttpGet("{id}")]
         public async Task<ActionResult<IEnumerable<Region>>> GetRegionBasedOnDistrictId(int id)
         {
