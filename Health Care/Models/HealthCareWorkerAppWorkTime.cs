d4fa00bb1e738e8eda4e1fb5d9e0645a5dcb176b
saeed-it <saeed2@live.com>
2021-07-28 19:09:41 +0300

    fix worker work time

--- Health Care/Models/HealthCareWorkerAppWorkTime.cs
+++ Health Care/Models/HealthCareWorkerAppWorkTime.cs
@@ -9,7 +9,7 @@ namespace Health_Care.Models
     {
         public int id { get; set; }
         public int userId { get; set; } // healthcareWorker id
-        public char shiftAM_PM { get; set; }
+        public string shiftAM_PM { get; set; }
         public int day { get; set; }
         public int startTime { get; set; }
         public int endTime { get; set; }
